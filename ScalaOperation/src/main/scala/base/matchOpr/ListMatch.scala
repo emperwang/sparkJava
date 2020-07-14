3cdbf94e0a1afc3dfef9b384ea9b00566906ab34
emperwang <544094478@qq.com>
2020-07-14 17:21:49 +0800

    match

---
+++ ScalaOperation/src/main/scala/base/matchOpr/ListMatch.scala
@@ -0,0 +1,22 @@
+package base.matchOpr
+
+object ListMatch {
+   def main(args:Array[String]):Unit={
+     var ll = List("--ip","192.168.30.10","--host","name2")
+     parse(ll)
+   }
+
+  def parse(args: List[String]):Unit = args match{
+    case ("--ip" | "-i"):: value :: tail =>
+      println("ip= " + value)
+      parse(tail)
+
+    case("--host"|"-h") :: value :: tail =>
+      println("host="+value)
+      parse(tail)
+
+    case Nil =>  // No-op
+
+  }
+
+}
